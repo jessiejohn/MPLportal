368e79fa0411386fbc2995ee720ee446ee8c4f4a
jessiejohn1 <jessie.john@unrulygroup.com>
2018-10-31 13:52:10 +0800

    firebase changes

--- html/js/key.js
+++ html/js/key.js
@@ -1,3 +1,18 @@
-$(function() {
-    $(".multiselect").chosen();
-});
+var app_fireBase = {};
+
+(function () {
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDwFkiLSI-3R3FMhpM93VcgitDpJQ_0CS4",
+    authDomain: "mpl-portal-f30d6.firebaseapp.com",
+    databaseURL: "https://mpl-portal-f30d6.firebaseio.com",
+    projectId: "mpl-portal-f30d6",
+    storageBucket: "mpl-portal-f30d6.appspot.com",
+    messagingSenderId: "993256942889"
+  };
+  firebase.initializeApp(config);
+
+  app_fireBase = firebase;
+
+}) ()
+
